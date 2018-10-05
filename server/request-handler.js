c7074e6dabe01352e43c943523ba8476ded3c1b0
kleekich <kangsik.lee@trizic.com>
2018-10-05 15:07:31 -0700

    Install nodemon

--- server/request-handler.js
+++ server/request-handler.js
@@ -10,7 +10,7 @@ this file and include it in basic-server.js so that it actually works.
 
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
-**************************************************************/
+**************************************************************/ 
 
 var requestHandler = function(request, response) {
   // Request and Response come from node's http module.
